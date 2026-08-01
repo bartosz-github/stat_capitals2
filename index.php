797a8a2354d71133b96bc35286e941bdc91a1d78
bartos-github <139777500+bartosz-github@users.noreply.github.com>
2026-08-01 01:25:17 +0100

    1

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>State Capitols</title>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+ 
+    <?php
+        $states = array (
+                array("Name","Abbr","LRGST","Date1",
+                array("Oklahoma","OK","unknown","16/11/1907",
+                array("New Mexico","NM","Albuquerque","06/01/1912",
+                array("Arizona","AZ","unknown","14/02/1912",
+                array("Alaska","AK","Anchorage","03/01/1959",
+                array("Hawaii","HI","unknown","21/08/1959",
+                array("Ohio","OH","unknown","01/03/1803",
+                array("Nebraska","NE","Omaha","1/3/1867",
+                array("Kentucky[B]","KY","Louisville","01/06/1792",
+                array("Tennessee","TN","unknown","01/06/1796",
+                array("Colorado","CO","unknown","1/8/1876",
+                array("Mississippi","MS","unknown","10/12/1817",
+                array("Wyoming","WY","unknown","10/7/1890",
+                array("Missouri","MO","Kansas City","10/8/1821",
+                array("Washington","WA","Seattle","11/11/1889",
+                array("Indiana","IN","unknown","11/12/1816",
+                array("Minnesota","MN","Minneapolis","11/5/1858",
+                array("Pennsylvania[B]","PA","Philadelphia","12/12/1787",
+                array("Alabama","AL","Huntsville","14/12/1819",
+                array("Oregon","OR","Portland","14/2/1859",
+                array("Maine","ME","Portland","15/3/1820",
+                array("Arkansas","AR","unknown","15/6/1836",
+                array("New Jersey","NJ","Newark","18/12/1787",
+                array("Georgia","GA","unknown","2/1/1788",
+                array("North Dakota","ND","Fargo","2/11/1889",
+                array("South Dakota","SD","Sioux Falls","2/11/1889",
+                array("West Virginia","WV","unknown","20/6/1863",
+                array("North Carolina","NC","Charlotte","21/11/1789",
+                array("New Hampshire","NH","Manchester","21/6/1788",
+                array("South Carolina","SC","Charleston","23/5/1788",
+                array("Virginia[B]","VA","Virginia Beach","25/6/1788",
+                array("Michigan","MI","Detroit","26/1/1837",
+                array("New York","NY","New York City","26/7/1788",
+                array("Iowa","IA","unknown","28/12/1846",
+                array("Maryland","MD","Baltimore","28/4/1788",
+                array("Kansas","KS","Wichita","29/1/1861",
+                array("Texas","TX","Houston","29/12/1845",
+                array("Rhode Island","RI","unknown","29/5/1790",
+                array("Wisconsin","WI","Milwaukee","29/5/1848",
+                array("Illinois","IL","Chicago","3/12/1818",
+                array("Florida","FL","Jacksonville","3/3/1845",
+                array("Idaho","ID","unknown","3/7/1890",
+                array("Louisiana","LA","New Orleans","30/4/1812",
+                array("Nevada","NV","Las Vegas","31/10/1864",
+                array("Utah","UT","unknown","4/1/1896",
+                array("Vermont","VT","Burlington","4/3/1791",
+                array("Massachusetts[B]","MA","unknown","6/2/1788",
+                array("Delaware","DE","Wilmington","7/12/1787",
+                array("Montana","MT","Billings","8/11/1889",
+                array("Connecticut","CT","Bridgeport","9/1/1788",
+                array("California","CA","Los Angeles","9/9/1850"
+                );
+
+    ?>
+
+  </body>
+</html>
