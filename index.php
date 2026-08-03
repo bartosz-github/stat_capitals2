b31b63c9132aa0dcd5918ebc0a0398e382bdf9af
bartos-github <139777500+bartosz-github@users.noreply.github.com>
2026-08-04 00:58:28 +0100

    1

--- index.php
+++ index.php
@@ -10,8 +10,34 @@
   <body>
  
     <?php
+      $host = "localhost";
+      $user = "**************";
+      $password = "**************";
+      $database = "statecapitols";
+
+      $connect = new mysqli($host, $user, $password, $database);
+
+      if($connect->error)
+        die("Failed to connect with database");
+      echo "Database connected!";
+
+      $sql = "SELECT id, State, Abbr, Capitol, Largest, Population FROM statecaps";
+      $result = $connect->query($sql);
 
 
+      echo "<br>";
+      echo "<br>";
+
+      if ($result->num_rows > 0) {
+        // Output data of each row
+        while($row = $result->fetch_assoc()) {
+          echo "id: " . $row["id"]. " - State: " . $row["State"]. "- Abbr" . $row["Abbr"] . "- Capitol" . $row["Capitol"] . "- Largest" . $row["Largest"] . "- Population" . $row["Population"] . "<br>";
+        }
+      } else {
+        echo "0 results";
+      }
+
+      $connect->close();
     ?>
 
   </body>
