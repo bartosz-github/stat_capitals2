935809564fb23d46a28c65ffe8558dc372477dd5
bartos-github <139777500+bartosz-github@users.noreply.github.com>
2026-08-06 02:44:22 +0100

    1

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
         die("Failed to connect with database");
       echo "Database connected!";
 
-      $sql = "SELECT id, State, Abbr, Capitol, Largest, Population FROM statecaps";
+      $sql = "SELECT id, State_name, Abbr, Capital, Most_pop, Population, 2nd_most_pop, 2nd_most_pop_pops, 3rd_most_pop, 3rd_most_pop_pops FROM statecaps";
       $result = $connect->query($sql);
 
 
@@ -31,7 +31,7 @@
       if ($result->num_rows > 0) {
         // Output data of each row
         while($row = $result->fetch_assoc()) {
-          echo "" . $row["id"]. " - " . $row["State"]. " - " . $row["Abbr"] . " - " . $row["Capitol"] . " - " . $row["Largest"] . "<br>";
+          echo "" . $row["id"]. " - " . $row["State_name"]." [". $row["Abbr"] ."] ". " - " . $row["Capital"] . " - " . $row["Most_pop"] ." [". $row["Population"] ."] ". " - " . $row["2nd_most_pop"] ." [". $row["2nd_most_pop_pops"] ."] ". " - " . $row["3rd_most_pop"] ." [". $row["3rd_most_pop_pops"] ."] ". "<br>";
         }
       } else {
         echo "0 results";
@@ -43,10 +43,10 @@
 <br>
 <h2>2do:</h2>
 <ol>
-  <li>Update database</li>
+  <li>Update database &#9989;</li>
   <li>button</li>
   <li>sort by highest / lowerst population</li>
-  <li>let something suggest few further changes/updates for now</li>
+  <li><small>let something suggest few further changes/updates for now</small></li>
 </ol>
   </body>
 </html>
